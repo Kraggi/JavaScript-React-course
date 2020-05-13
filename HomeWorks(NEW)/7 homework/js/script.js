a5bf48fa8e3b9cbdf91d6754c590e7eda6d3c9a1
German Yanovich <germanyanovich@gmail.com>
2020-05-13 23:17:59 +0300

    add modal backend

--- HomeWorks(NEW)/7 homework/js/script.js
+++ HomeWorks(NEW)/7 homework/js/script.js
@@ -49,11 +49,11 @@ window.addEventListener('DOMContentLoaded', function () {
       seconds = Math.floor((t / 1000) % 60);
 
     return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
     };
   }
 
@@ -103,7 +103,7 @@ window.addEventListener('DOMContentLoaded', function () {
     clearInterval(modalTimerId);
   }
 
-  modalTrigger.forEach(item => {
+  modalTrigger.forEach((item) => {
     item.addEventListener('click', openModal);
   });
 
@@ -128,7 +128,10 @@ window.addEventListener('DOMContentLoaded', function () {
   const modalTimerId = setTimeout(openModal, 5000);
 
   function showModalByScroll() {
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
       openModal();
       window.removeEventListener('scroll', showModalByScroll);
     }
@@ -161,7 +164,7 @@ window.addEventListener('DOMContentLoaded', function () {
         this.element = 'menu__item';
         element.classList.add(this.element);
       } else {
-        this.classes.forEach(className => element.classList.add(className));
+        this.classes.forEach((className) => element.classList.add(className));
       }
 
       element.innerHTML = `
@@ -181,37 +184,89 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
+    'img/tabs/vegy.jpg',
+    'vegy',
     `Меню "Фитнес"`,
     `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
     свежих овощей и фруктов. Продукт активных и здоровых людей. Это
     абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     6,
-    '.menu .container',
+    '.menu .container'
   ).render();
 
   new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
+    'img/tabs/post.jpg',
+    'post',
     `Меню "Постное"`,
     `Меню “Постное” - это тщательный подбор ингредиентов: полное
     отсутствие продуктов животного происхождения, молоко из миндаля,
     овса, кокоса или гречки, правильное количество белков за счет тофу
     и импортных вегетарианских стейков.`,
     9,
-    '.menu .container',
+    '.menu .container'
   ).render();
 
   new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
+    'img/tabs/elite.jpg',
+    'elite',
     `Меню “Премиум”`,
     `В меню “Премиум” мы используем не только красивый дизайн упаковки,
     но и качественное исполнение блюд. Красная рыба, морепродукты,
     фрукты - ресторанное меню без похода в ресторан!`,
     15,
-    '.menu .container',
+    '.menu .container'
   ).render();
 
+  // Forms
+
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...',
+  };
+
+  forms.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      request.setRequestHeader('Content-Type', 'application/json');
+      const formData = new FormData(form);
+
+      const object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+
+      request.send(json);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+          form.reset();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
